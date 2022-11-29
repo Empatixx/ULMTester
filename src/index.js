8589eed1333a4f19c8ba0147499bcf81a33695ec
Jirka <kingofmatrix.live@gmail.com>
2022-11-29 21:02:20 +0100

    gh issues

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Tester from './Tester';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route , Routes } from 'react-router-dom';
 import Cheatsheet from './Cheatsheet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/test" exact element={<Tester />} />
         <Route path="/" exact element={<Cheatsheet />} />
       </Routes>
-    </Router>
+    </HashR>
   </React.StrictMode>
 );
 
